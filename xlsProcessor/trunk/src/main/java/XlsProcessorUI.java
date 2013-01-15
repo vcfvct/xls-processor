a474b53ee5dbcf889d2575456b89b45d0bd79182
vcfvct@gmail.com <vcfvct@gmail.com@4685b321-7bcf-5ab1-1755-015738ca30fd>
2013-01-15 20:13:14 +0000

    add Swing Gui JfileChooser Progress bar

--- xlsProcessor/trunk/src/main/java/XlsProcessorUI.java
+++ xlsProcessor/trunk/src/main/java/XlsProcessorUI.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -15,23 +16,34 @@ public class XlsProcessorUI
     private JPanel panel1;
     private JButton startProcessButton;
     private JFileChooser fileChooser;
+    private JProgressBar progressBar1;
+    private JTextArea progressInfo;
 
     public XlsProcessorUI()
     {
+        progressBar1.setStringPainted(true);
+        progressBar1.setMinimum(0);
+        progressBar1.setMaximum(100);
+        progressInfo.setEnabled(false);
         fileChooser.setControlButtonsAreShown(false);
         fileChooser.setName("XLS Processor");
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.setFileFilter(new Xlsfilter());
-        fileChooser.addPropertyChangeListener(new PropertyChangeListener() {
-            public void propertyChange(PropertyChangeEvent evt) {
-                if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(evt.getPropertyName())) {
+        fileChooser.addPropertyChangeListener(new PropertyChangeListener()
+        {
+            public void propertyChange(PropertyChangeEvent evt)
+            {
+                if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(evt.getPropertyName()))
+                {
                     File file = (File) evt.getNewValue();
 
-                    if (file != null && file.isFile() && file.getName().contains("xls")) {
+                    if (file != null && file.isFile() && file.getName().contains("xls"))
+                    {
                         startProcessButton.setEnabled(true);
 
                     }
-                    else if ( file != null ) {
+                    else if (file != null)
+                    {
                         startProcessButton.setEnabled(false);
                     }
                 }
@@ -46,29 +58,45 @@ public class XlsProcessorUI
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                File file = fileChooser.getSelectedFile();
-                if (file == null)
+                progressBar1.setValue(0);
+                progressInfo.setText("");
+                Runnable runner = new Runnable()
                 {
-                    return;
-                }
-                ReturnGenerator generator = new ReturnGenerator(file);
-                try
-                {
-                    generator.generate();
-                }
-                catch (IOException e1)
-                {
-                    e1.printStackTrace();
-                }
+                    @Override
+                    public void run()
+                    {
+                        File file = fileChooser.getSelectedFile();
+                        if (file == null)
+                        {
+                            return;
+                        }
+                        ReturnGenerator generator = new ReturnGenerator(file);
+                        try
+                        {
+                            generator.generate(progressBar1, progressInfo);
+                        }
+                        catch (IOException e1)
+                        {
+                            e1.printStackTrace();
+                        }
+                    }
+                };
+                Thread t = new Thread(runner, "Code Executer");
+                t.start();
+
             }
         });
+    }
 
-
+    public void updateBar(int newValue)
+    {
+        progressBar1.setValue(newValue);
     }
 
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("XlsProcessorUI");
+        frame.setPreferredSize(new Dimension(800,600));
         frame.setContentPane(new XlsProcessorUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
